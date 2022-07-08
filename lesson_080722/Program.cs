1c35a721567ff085abd9c8fd851ac9bc45459467
«George» <prior21@mail.ru>
2022-07-09 02:14:11 +0300

    added Task 2

--- lesson_080722/Program.cs
+++ lesson_080722/Program.cs
@@ -7,12 +7,61 @@
 // 4 -> 16
 // -3 -> 9
 // -7 -> 49
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num * num;
+Console.WriteLine($"Квадрат числа {num} = {res}");
 
-int a = 4,
-    b = -3,
-    c = -7;
+//int a = 4,
+//    b = - 3,
+//    c = - 7;
 
-Console.WriteLine(a*a);
-Console.WriteLine(b*b);
-Console.WriteLine(c*c);
+//Console.WriteLine(a*a);
+//Console.WriteLine(b*b);
+//Console.WriteLine(c*c);
+
+//int  num = 4;
+//double d = 4.6;
+//int res = num * num;
+//Console.WriteLine(res);
+
+// Task 2
+//1. Напишите программу, которая на вход принимает два
+//числа и проверяет, является ли первое число квадратом
+//второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+
+//int num = Convert.ToInt32(Console.ReadLine());
+//int num2  = Convert.ToInt32(Console.ReadLine());
+int a=25, b=5;
+
+if(a == b * b);
+    {
+    Console. Write(true);
+    }
+
+Console.Write("Write a number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+if (n < 0)
+{
+     i = n;
+    n = n * -1;
+}
+else
+{
+   i = -n;
+}
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i++;
+}
 
